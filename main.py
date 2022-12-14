2adf7a7e821945c09db94b847c875a6e28bcbf5e
hanisalah <hanisalah@hotmail.com>
2022-12-14 15:33:09 +0200

    r0.1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import cv2
+import extcolors
 import numpy as np
+from PIL import Image
 import streamlit as st
 from io import BytesIO
 from copy import deepcopy
 from zipfile import ZipFile
 from datetime import datetime
-from sklearn.utils import shuffle
-from sklearn.cluster import KMeans
 
 def ix_change(mode=0):
     """manipulate index of image to be shown based on clicks from relevant prev/next buttons"""
@@ -50,59 +50,119 @@ def clean(img):
     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, cv2.BORDER_REPLICATE)
     return image
 
-def recreate_image(cookbook, labels, w, h):
-    vfunc = lambda x: cookbook[labels[x]]
-    image = vfunc(np.arange(h*w))
-    image = np.resize(image, (h, w, cookbook.shape[1]))
-    return image
-
-def quantize(img, num_clrs):
-    H, W, D = img.shape[:3]
-    image = np.reshape(img, (W*H, D))
-    image_array_sample = shuffle(image, random_state=0)[:1000]
-    kmeans = KMeans(n_clusters=num_clrs).fit(image_array_sample)
-    labels = kmeans.predict(image)
-    image = recreate_image(kmeans.cluster_centers_, labels, W, H)
-    return image, kmeans.cluster_centers_
-
-def generate(img, ix, num_clrs):
+def generate(img, ix, num_clrs, progress_bar):
     """generate 'color by number' image from the passed image, index and number of color clusters"""
     inp = img[ix]['zoom'] #resize is performed separately for interface convenience
     #image = inp #image = resize(inp)
     image = clean(inp)
-    image = np.array(image, dtype='uint8')/255
-    image, colors = quantize(image, num_clrs)
-    canvas = np.ones(image.shape[:3], dtype="uint8")*255
-    colormap = [[int(c*255) for c in col] for col in colors]
-    h, w = canvas.shape[:2]
-    map_img, font_scale = draw_colormap(colormap, h, w)
-    for ind, color in enumerate(colors):
-        #colormap.append([int(c*255) for c in color])
-        mask = cv2.inRange(image, color, color)
-        cnts = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        for contour in cnts:
-            _, _, width_ctr, height_ctr = cv2.boundingRect(contour)
-            if width_ctr > 10 and height_ctr > 10 and cv2.contourArea(contour, True) < -100:
-                cv2.drawContours(canvas, [contour], -1, 0, 1)
-                #Add label
-                #txt_x, txt_y = contour[0][0]
-                M = cv2.moments(contour) #use contour centroid
-                txt_x = int(M["m10"] / M["m00"])
-                txt_y = int(M["m01"] / M["m00"])
-                cv2.putText(canvas, '{:d}'.format(ind + 1), (txt_x, txt_y + 15), cv2.FONT_HERSHEY_SIMPLEX, font_scale, 0, 1) #originally font scale = 0.5
-    img[ix]['out'] = canvas
+    colormap = img[ix]['colormap_tuples']
+    colortxt = img[ix]['colormap_text']
+    quantized = quantize(image, colormap, colortxt)
+    cbn = CBN(quantized, colormap, progress_bar)
+    map_img, fontscale = draw_colormap(colormap, colortxt, inp.shape[0], inp.shape[1])
+    img[ix]['quantized'] = quantized
+    img[ix]['out'] = cbn
     img[ix]['color_map'] = map_img
-    img[ix]['out_color_map'] = np.vstack((canvas,map_img))
+    img[ix]['out_color_map'] = np.vstack((cbn,map_img))
 
-def draw_colormap(colormap, h, w):
+def blank_colormap(img, ix, num_clrs, tolerance):
+    """generate list of distinct colors in an image"""
+    inp = deepcopy(img[ix]['zoom'])
+    image = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    colors, pixel_count = extcolors.extract_from_image(image, tolerance=tolerance, limit=num_clrs)
+    colors = [i[::-1] for (i,j) in colors] #palette of distinct colors: list of (B,G,R) tuples
+    img[ix]['colormap_tuples'] = colors
+    img[ix]['colormap_text'] = ['{}'.format(c[::-1]) for c in colors]
+
+def quantize(img, colormap, colortext):
+    """redraw image using colors palette"""
+    image = img.reshape(img.shape[0],img.shape[1],1,3)
+    colors_container = np.ones(shape=[image.shape[0],image.shape[1],len(colormap),3])
+    colors = [list(c) for c in colormap] #palette of distinct colors list of lists
+    for i,color in enumerate(colors):
+        colors_container[:,:,i,:] = color
+    shape = image.shape[:2]
+    total_shape = shape[0]*shape[1]
+    distances = np.sqrt(np.sum((colors_container-image)**2,axis=3))
+    min_index = np.argmin(distances,axis=2).reshape(-1)
+    natural_index = np.arange(total_shape)
+    reshaped_container = colors_container.reshape(-1,len(colors),3)
+    color_view = reshaped_container[natural_index,min_index].reshape(shape[0],shape[1],3)
+    color_view = color_view.astype('uint8')
+    return color_view
+
+def CBN(img, colors, progress_bar):
+    canvas = np.ones((img.shape[0],img.shape[1],img.shape[2]),dtype='uint8') * 255 #used to draw the final CBN image
+    #used to draw a negative (black) of the contour to exclude areas not suitable to place text of next contour.
+    negative = np.ones((img.shape[0],img.shape[1]),dtype='uint8') * 255
+    #release contours from its hierarchy and have it as an unnested list of contours
+    contours = []
+    for ind, color in enumerate(colors):
+        color = np.asarray(color, dtype='uint8')
+        mask = cv2.inRange(img, color, color)
+        #mask = clean(img)
+        cnts,hier = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        c = [{'cnt':cnt,'ind':ind+1} for cnt in cnts if
+            cv2.boundingRect(cnt)[2]>10
+            and cv2.boundingRect(cnt)[3]>10
+            and cv2.contourArea(cnt,False)>100]
+        contours.extend(c)
+    contours = sorted(contours, key= lambda x:cv2.contourArea(x['cnt'],False), reverse=False) #arcLength can also be used
+    txts = tuple([str(x['ind']) for x in contours])
+    contours = tuple([x['cnt'] for x in contours])
+
+    for i, cnt in enumerate(contours):
+        cv2.drawContours(canvas,[cnt],-1,0,thickness=1)
+
+        #identify suitable place to put text
+        cnt_x, cnt_y, cnt_w, cnt_h = cv2.boundingRect(cnt)
+        #patch = negative[cnt_y:cnt_y+cnt_h, cnt_x:cnt_x+cnt_w, :] #get a patch from the negative
+        patch = negative[cnt_y:cnt_y+cnt_h, cnt_x:cnt_x+cnt_w] #get a patch from the negative
+        font_scale=1
+        flag = True
+        while flag:
+            if font_scale >0.5: #try to find a suitable place to put the text with font scale from 1 to 0.5
+                txt_w, txt_h = cv2.getTextSize(txts[i], cv2.FONT_HERSHEY_SIMPLEX, font_scale, 1)[0] #get the text size in w x h
+                if patch.shape[0]>txt_h and patch.shape[1]>txt_w: #check patch is bigger than text
+
+                    #select indices that when considered as a top-left coordinate for text result in complete white box that is inside the contour
+                    white_patches = np.argwhere(np.lib.stride_tricks.sliding_window_view(patch,(txt_h,txt_w)).all(axis=(-2,-1)))
+                    white_patches = white_patches.tolist()
+                    white_patches = [x for x in white_patches if
+                                    cv2.pointPolygonTest(cnt, (x[1]+cnt_x,x[0]+cnt_y), False)>0 #TL of text in contour
+                                    and cv2.pointPolygonTest(cnt, (x[1]+cnt_x+txt_w,x[0]+cnt_y), False)>0 #TR of text in contour
+                                    and cv2.pointPolygonTest(cnt, (x[1]+cnt_x+txt_w,x[0]+cnt_y+txt_h), False)>0 #BR of text in contour
+                                    and cv2.pointPolygonTest(cnt, (x[1]+cnt_x,x[0]+cnt_y+txt_h), False)>0 ] #BL of text in contour
+
+                    if len(white_patches)>0: # if there are top-left coordinates found, use the first coordinate (any one can be as good) to place text
+                        txt_x = white_patches[0][1]+cnt_x
+                        txt_y = white_patches[0][0]+cnt_y+txt_h
+                        cv2.putText(canvas, txts[i], (txt_x, txt_y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, 0, 1)
+                        flag = False
+                    else: #no top-left coordinates found, decrease font scale and try again
+                        font_scale -=0.1
+                else: #patch is smaller than text, decrease font and try again
+                    font_scale -=0.1
+            else: #we reached minimum possible font size. Place text at centroid of contour
+                M = cv2.moments(cnt) #use contour centroid
+                txt_x = int(M["m10"] / M['m00'])
+                txt_y = int(M["m01"] / M['m00'])
+                cv2.putText(canvas, txts[i], (txt_x, txt_y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 0, 1)
+                flag= False
+
+        cv2.drawContours(negative,[cnt],-1,0,thickness=cv2.FILLED) #fill the contour in negative with black (to create negative)
+        progress_bar.progress(i/len(contours))
+    return canvas
+
+def draw_colormap(colormap, colortext, h, w):
     """Plot or save the colormap as a picture for the user"""
     num_colors = len(colormap)
     num_patches = 4 if num_colors>=4 else num_colors #number of patches in a row
     font_face = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1
     font_thick = 3
-    clr_txt = ['{}: {}'.format(i+1,c[::-1]) for i,c in enumerate(colormap)]
+    clr_txt = ['{}: {}'.format(i+1,c) for i,c in enumerate(colortext)]
     flag = True
     max_txt, max_clr, big_space, small_space = [],[], [],[]
     while flag:
@@ -140,7 +200,7 @@ def draw_colormap(colormap, h, w):
             txt = np.ones((max_txt[1], max_txt[0], 3), dtype='uint8')*255
             if ix < len(colormap):
                 clr = cv2.rectangle(clr, (0+clr_offset[0],0+clr_offset[1]), (max_clr[0]-clr_offset[0],max_clr[1]-clr_offset[1]), colormap[ix], thickness=-1)
-                clr = cv2.rectangle(clr, (0+clr_offset[0],0+clr_offset[1]), (max_clr[0]-clr_offset[0],max_clr[1]-clr_offset[1]), (0,0,0), thickness=1)
+                clr = cv2.rectangle(clr, (0+clr_offset[0],0+clr_offset[1]), (max_clr[0]-clr_offset[0],max_clr[1]-clr_offset[1]), (0,0,0), thickness=3)
                 txt = cv2.putText(txt, clr_txt[ix], (0,max_txt[1]-5), font_face, font_scale, (0,0,0), thickness=1)
             if col == 0:
                 rowbuild = np.hstack((small,clr,txt))
@@ -148,10 +208,11 @@ def draw_colormap(colormap, h, w):
                 rowbuild = np.hstack((rowbuild,big,clr,txt,small))
             else:
                 rowbuild = np.hstack((rowbuild,big,clr,txt))
+        v_space = np.ones((5, rowbuild.shape[1], 3), dtype='uint8')*255
         if mapbuild is None:
-            mapbuild = rowbuild
+            mapbuild = np.vstack((v_space,rowbuild, v_space))
         else:
-            mapbuild = np.vstack((mapbuild, rowbuild))
+            mapbuild = np.vstack((mapbuild, rowbuild, v_space))
     pad = w - mapbuild.shape[1]
     pad = np.ones((mapbuild.shape[0],pad,3), dtype='uint8')*255
     mapbuild = np.hstack((mapbuild,pad))
@@ -166,7 +227,7 @@ def main():
             del st.session_state[key]
 
     # defining session state variables
-    img_dict = {'img_name':'', 'inp':'', 'zoom':'', 'out':None, 'color_map': None, 'out_color_map':None}
+    img_dict = {'img_name':'', 'inp':'', 'zoom':'', 'out':None, 'quantized': None, 'color_map': None, 'out_color_map':None, 'colormap_tuples':[], 'colormap_text':[]}
     if 'imgs' not in st.session_state.keys():
         st.session_state.imgs=[]
     if 'imgs_ix' not in st.session_state.keys():
@@ -196,7 +257,44 @@ def main():
                 st.session_state.imgs_ix +=1
 
     # add navigation controls
-    col01, col02, col03, col04, col05 = st.columns([2,1,2,2,2])
+    col01, col02, col03, col04, col05 = st.columns([2,1,2,2,2]) #prev, imgs_ix, next, generate, save_all
+    if st.session_state.imgs_ix>=0:
+        progress = st.progress(0)
+        zoom_factor = st.select_slider('Zoom', options=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.5,2,2.5,3], value=1)
+        resize(st.session_state.imgs, st.session_state.imgs_ix, zoom_factor)
+        cntnr01 = st.expander('ColorMap Controls', expanded=True)
+        with cntnr01:
+            num_clrs_slider = st.slider('Select number of colors required', min_value=1, max_value=50, value=10, step=int(1))
+            tolerance_slider = st.slider('Select tolerance to group similar colors', min_value=0, max_value=100, value=32, step=int(1))
+            _,col101, col102,_ = st.columns([1,1,1,1]) # 101: color map button, 102: update caption button
+
+            col201, col202 = st.columns([1,2]) #201: color patch, 202: caption
+            if len(st.session_state.imgs[st.session_state.imgs_ix]['colormap_tuples'])>0:
+                txt_lst = ['']* len(st.session_state.imgs[st.session_state.imgs_ix]['colormap_text'])
+                with col201:
+                    for color in st.session_state.imgs[st.session_state.imgs_ix]['colormap_tuples']:
+                        st.write('')
+                        st.write('')
+                        st.image(cv2.rectangle(np.zeros((40,40,3),dtype='uint8'),(2,2),(38,38),color, thickness=-1), channels='BGR')
+                with col202:
+                    for i, text in enumerate(st.session_state.imgs[st.session_state.imgs_ix]['colormap_text']):
+                        placeholder ='{}'.format(st.session_state.imgs[st.session_state.imgs_ix]['colormap_tuples'][i][::-1])
+                        key = '{}-{}'.format(st.session_state.imgs[st.session_state.imgs_ix]['img_name'],i)
+                        txt_lst[i] = st.text_input('Caption '+str(i),value = text, placeholder=placeholder, key=key)
+                        if len(txt_lst[i])==0:
+                            txt_lst[i] = placeholder
+            with col101:
+                blank_clr_btn = st.button('ColorMap', on_click=blank_colormap, args=(st.session_state.imgs, st.session_state.imgs_ix, num_clrs_slider, tolerance_slider))
+            with col102:
+                update_caption_dsbl = True if len(st.session_state.imgs[st.session_state.imgs_ix]['colormap_tuples']) == 0 else False
+                is_update_caption = st.button('Update Caption', disabled=update_caption_dsbl)
+                if is_update_caption:
+                    st.session_state.imgs[st.session_state.imgs_ix]['colormap_text'] = txt_lst
+    else:
+        num_clrs_slider = 0
+        zoom_factor = 1
+        tolerance_slider=32
+
     with col01:
         prev_dsbl = True if st.session_state.imgs_ix <=0 else False
         is_prev = st.button('Prev', on_click=ix_change, kwargs={'mode':-1}, disabled=prev_dsbl)
@@ -205,16 +303,9 @@ def main():
     with col03:
         next_dsbl = True if st.session_state.imgs_ix >= len(st.session_state.imgs)-1 else False
         is_next = st.button('Next', on_click=ix_change, kwargs={'mode':1}, disabled=next_dsbl)
-    if st.session_state.imgs_ix>=0:
-        zoom_factor = st.select_slider('Zoom', options=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.5,2,2.5,3], value=1)
-        num_clrs_slider = st.slider('Select number of color clusters required', min_value=1, max_value=50, value=10, step=int(1))
-        resize(st.session_state.imgs, st.session_state.imgs_ix, zoom_factor)
-    else:
-        num_clrs_slider = 0
-        zoom_factor = 1
     with col04:
         gen_dsbl = True if len(st.session_state.imgs)<=0 else False
-        is_gen = st.button('Generate', on_click=generate, args=(st.session_state.imgs, st.session_state.imgs_ix, num_clrs_slider), disabled=gen_dsbl)
+        is_gen = st.button('Generate', on_click=generate, args=(st.session_state.imgs, st.session_state.imgs_ix, num_clrs_slider, progress), disabled=gen_dsbl)
         if is_gen:
             st.balloons()
     with col05:
@@ -230,19 +321,26 @@ def main():
             h,w = st.session_state.imgs[st.session_state.imgs_ix]['zoom'].shape[:2]
             st.write('Resolution: {} x {}'.format(w,h))
     with col12:
-        # display the output image
-        if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['out'] is not None:
-            st.image(st.session_state.imgs[st.session_state.imgs_ix]['out'], channels='BGR')
+        # display the quantized image
+        if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['quantized'] is not None:
+            st.image(st.session_state.imgs[st.session_state.imgs_ix]['quantized'], channels='BGR')
+            st.write('Image redrawn using ColorMap')
 
     col21, col22 = st.columns(2)
     with col21:
-        # display the color_map
-        if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['color_map'] is not None:
-            st.image(st.session_state.imgs[st.session_state.imgs_ix]['color_map'], channels='BGR')
+        # display the CBN image
+        if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['out'] is not None:
+            st.image(st.session_state.imgs[st.session_state.imgs_ix]['out'], channels='BGR')
+            st.write('Color By Number Image')
     with col22:
         # display the output image + color_map combined
         if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['out_color_map'] is not None:
             st.image(st.session_state.imgs[st.session_state.imgs_ix]['out_color_map'], channels='BGR')
+            st.write('Final Image with ColorMap')
+    # display the color map
+    if st.session_state.imgs_ix>=0 and st.session_state.imgs[st.session_state.imgs_ix]['color_map'] is not None:
+        st.image(st.session_state.imgs[st.session_state.imgs_ix]['color_map'], channels='BGR')
+        st.write('ColorMap')
 
 if __name__ == '__main__':
     main()
